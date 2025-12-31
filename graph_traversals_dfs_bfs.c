f016d24aa3ac6b3c5bbcf3cdced21df09e2e237d
esenygt0 <yigitesengul951@gmail.com>
2025-12-31 17:33:50 +0300

    graph_traversals_dfs_bfs.c
    Updated DFS implementation to use a recursive approach and improved comments.

--- graph_traversals_dfs_bfs.c
+++ graph_traversals_dfs_bfs.c
@@ -1,12 +1,47 @@
-/* * BAŞLIK: DFS (Depth First Search) Uygulaması
- * AÇIKLAMA: Yığın yapısını kullanarak grafın derinliklerine doğru arama yapar.
+/* 
+ * BAŞLIK: DFS (Depth First Search) Uygulaması
+ * AÇIKLAMA: Rekürsif yaklaşım kullanarak graf üzerinde
+ * derinlik öncelikli arama (DFS) işlemini gerçekleştirir.
  */
+
 #include <stdio.h>
 
-void DFS(int adj[10][10], int visited[10], int v, int n) {
+#define MAX 10
+
+/* 
+ * FONKSİYON: DFS
+ * AÇIKLAMA: Verilen graf üzerinde DFS traversal yapar.
+ */
+void DFS(int adj[MAX][MAX], int visited[MAX], int v, int n) {
     visited[v] = 1;
     printf("%d ", v);
-    for (int i = 0; i < n; i++)
-        if (adj[v][i] && !visited[i]) DFS(adj, visited, i, n);
 
+    for (int i = 0; i < n; i++) {
+        if (adj[v][i] == 1 && !visited[i]) {
+            DFS(adj, visited, i, n);
+        }
+    }
+}
+
+/* 
+ * FONKSİYON: main
+ * AÇIKLAMA: DFS algoritmasını test eder.
+ */
+int main() {
+    int n = 5;
+    int visited[MAX] = {0};
+
+    int adj[MAX][MAX] = {
+        {0,1,1,0,0},
+        {1,0,0,1,0},
+        {1,0,0,1,1},
+        {0,1,1,0,0},
+        {0,0,1,0,0}
+    };
+
+    printf("DFS Traversal: ");
+    DFS(adj, visited, 0, n);
+    printf("\n");
+
+    return 0;
 }
