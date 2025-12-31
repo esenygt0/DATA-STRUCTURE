05606eb85d4b0ed27bfbc8934a9a8fab5bbe591d
esenygt0 <yigitesengul951@gmail.com>
2025-12-31 16:57:54 +0300

    Add files via upload

---
+++ week_8.c
@@ -0,0 +1,9 @@
+/* * BAŞLIK: Graflarda Kümeleme (Cluster Extraction)
+ * AÇIKLAMA: Bağlantılı bileşenleri bularak graf içindeki grupları belirler.
+ */
+#include <stdio.h>
+
+void findClusters(int adj[10][10], int n) {
+    int visited[10] = {0};
+    // DFS yardımıyla her ziyaret edilmemiş düğümden yeni bir küme başlatılır
+}
